43ae34bc7e45b5f3145df209eac9b4741672002d
Npeterson20 <Npeterson@students.unwsp.edu>
2024-09-13 22:50:48 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,5 +1,9 @@
 def personal_information():
-    print('...')
+    # Displaying personal information
+print("Name: Nolan Peterson")
+print("Address: Owatonna, Minnesota, 55060")
+print("Telephone Number: (507) 686-0767")
+print("College Major: Computer Science")
 
 
 # Line which calls the above function.
